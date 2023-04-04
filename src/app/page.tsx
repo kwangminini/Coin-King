b15aeadae801def8874614d0361b25972ea67ec5
kwangminini <rhkdals7362@gmail.com>
2023-04-04 23:54:09 +0900

    feat: homepage UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
+import Card from '@/components/Card'
+import { coins, ICoins } from '@/constants/coins'
+
 export default function Home() {
   return (
-    <div className="w-[800px] mx-auto border-x border-main-border-color h-[calc(100vh-60px)]">
+    <div className="w-full max-w-[800px] mx-auto border-x border-main-border-color h-[calc(100vh-60px)]">
       <p className="text-center pt-12 font-normal text-2xl text-main-font-color">
         실시간 시세 정보
       </p>
+      <div className="flex flex-col items-center mt-[45px] gap-9">
+        {coins &&
+          coins.map(({ icon, name, unit }: ICoins) => (
+            <Card icon={icon} name={name} unit={unit} key={name} />
+          ))}
+      </div>
     </div>
   )
 }
