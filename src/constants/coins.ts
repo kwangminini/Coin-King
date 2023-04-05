470f2f2eab26fc2181dbe709f9871f37e6516a3a
kwangminini <rhkdals7362@gmail.com>
2023-04-06 00:32:00 +0900

    refactor: change coin state structure

--- src/constants/coins.ts
+++ src/constants/coins.ts
@@ -1,21 +1,25 @@
-export interface ICoins {
+export interface ICoinInfo {
+  id: string
   icon: string
   name: string
   unit: string
 }
 
-export const coins: ICoins[] = [
+export const coinInfo: ICoinInfo[] = [
   {
+    id: 'btc',
     icon: '/btc.svg',
     name: '비트코인',
     unit: 'BTC/KRW',
   },
   {
+    id: 'eth',
     icon: '/eth.svg',
     name: '이더리움',
     unit: 'ETH/KRW',
   },
   {
+    id: 'xrp',
     icon: '/xrp.svg',
     name: '리플',
     unit: 'XRP/KRW',
