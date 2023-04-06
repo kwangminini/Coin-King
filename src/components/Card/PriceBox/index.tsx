b873a3268fa18ce8dd9c65c2b1fab8bffa0b40dd
kwangminini <rhkdals7362@gmail.com>
2023-04-07 00:27:39 +0900

    refactor: delete websocket logic and getCoinState logic

--- src/components/Card/PriceBox/index.tsx
+++ src/components/Card/PriceBox/index.tsx
@@ -1,53 +1,22 @@
 'use client'
 
-import {
-  btcInfoState,
-  ethInfoState,
-  ICoinState,
-  xrpInfoState,
-} from '@/atoms/coinsAtom'
-import useWebsocket from '@/hooks/useWebsocket'
-import { useEffect, useState } from 'react'
-import { useRecoilValue } from 'recoil'
+import { ICoinState } from '@/atoms/coinsAtom'
+import useGetCoinState from '@/hooks/useGetCoinState'
 
 interface IProp {
   coinId: string
 }
 
 export default function PriceBox({ coinId }: IProp) {
-  const { socket, isConnected, socketData } = useWebsocket()
-  const btcInfo = useRecoilValue(btcInfoState)
-  const ethInfo = useRecoilValue(ethInfoState)
-  const xrpInfo = useRecoilValue(xrpInfoState)
-
-  const getCoinState = (coinId: string): ICoinState | undefined => {
-    switch (coinId) {
-      case 'btc':
-        return btcInfo
-      case 'eth':
-        return ethInfo
-      case 'xrp':
-        return xrpInfo
-      default:
-        return
-    }
-  }
+  const curCoin = useGetCoinState(coinId)
 
   return (
     <div className="ml-auto flex flex-col items-end">
-      <p
-        className={`text-base font-semibold ${getTextColor(
-          getCoinState(coinId)
-        )}`}
-      >
-        {getCoinState(coinId)?.tp?.toLocaleString('ko-KR')}
+      <p className={`text-base font-semibold ${getTextColor(curCoin)}`}>
+        {curCoin?.tp?.toLocaleString('ko-KR')}
       </p>
-      <p
-        className={`font-semibold text-sm ${getTextColor(
-          getCoinState(coinId)
-        )}`}
-      >
-        {getCoinState(coinId)?.scr}%
+      <p className={`font-semibold text-sm ${getTextColor(curCoin)}`}>
+        {curCoin?.scr}%
       </p>
     </div>
   )
