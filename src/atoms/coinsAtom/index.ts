f5932850ad4a2247aa9b4d4234e9219ea8589d23
kwangminini <rhkdals7362@gmail.com>
2023-04-06 23:46:20 +0900

    refactor: delete duplicate variable

--- src/atoms/coinsAtom/index.ts
+++ src/atoms/coinsAtom/index.ts
@@ -1,40 +1,35 @@
 import { atom } from 'recoil'
 
+export interface ICoinState {
+  tp?: number //현재가
+  c?: 'RISE' | 'EVEN' | 'FALL' //전일대비 비교 (RISE => 상승, EVEN => 보합, FALL => 하락)
+  scr?: number //전일대비 퍼센트
+}
+
 const suffix = 'InfoState'
 
+const defaultState = {
+  tp: undefined,
+  c: undefined,
+  scr: undefined,
+}
 const atomKey = {
   btc: 'btc' + suffix,
   eth: 'eth' + suffix,
   xrp: 'xrp' + suffix,
 }
 
-export interface ICoinState {
-  tp?: number //현재가
-  c?: 'RISE' | 'EVEN' | 'FALL' //전일대비 비교 (RISE => 상승, EVEN => 보합, FALL => 하락)
-  scr?: number //전일대비 퍼센트
-}
 export const btcInfoState = atom<ICoinState>({
   key: atomKey.btc,
-  default: {
-    tp: undefined,
-    c: undefined,
-    scr: undefined,
-  },
+  default: defaultState,
 })
 
 export const ethInfoState = atom<ICoinState>({
   key: atomKey.eth,
-  default: {
-    tp: undefined,
-    c: undefined,
-    scr: undefined,
-  },
+  default: defaultState,
 })
+
 export const xrpInfoState = atom<ICoinState>({
   key: atomKey.xrp,
-  default: {
-    tp: undefined,
-    c: undefined,
-    scr: undefined,
-  },
+  default: defaultState,
 })
