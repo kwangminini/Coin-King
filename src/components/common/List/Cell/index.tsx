edec54dcae1b344b4d21e3738c25a11595889069
kwangminini <rhkdals7362@gmail.com>
2023-04-09 18:48:57 +0900

    refactor: 데이터 prop 삭제

--- src/components/common/List/Cell/index.tsx
+++ src/components/common/List/Cell/index.tsx
@@ -1,19 +1,9 @@
 interface IProp {
   children?: React.ReactNode //데이터가 하나의 문자열이 아닐 경우
-  data?: string //데이터가 하나의 문자일경우
   style?: string
 }
 
-export default function Cell({ data, children, style }: IProp) {
-  //데이터가 하나의 문자열일 경우
-  if (data) {
-    return (
-      <div className={`flex items-center w-full ${style ? style : ''}`}>
-        {data}
-      </div>
-    )
-  }
-  //데이터가 하나의 문자열로 끝나지 않는 경우
+export default function Cell({ children, style }: IProp) {
   return (
     <div className={`flex items-center w-full ${style ? style : ''}`}>
       {children}
