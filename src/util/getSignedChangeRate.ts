ef1458db76c18803d3b9691bd635e8d8d1193d80
kwangminini <rhkdals7362@gmail.com>
2023-04-09 18:57:52 +0900

    feat: 전일대비 변동율 반환해주는 util 함수

---
+++ src/util/getSignedChangeRate.ts
@@ -0,0 +1,14 @@
+/**
+ * 전일 대비 증감율 가공 함수
+ * @param {number | undefined} scr - 전일 대비 증감율
+ * @returns '+전일 대비 증감율%' or '-저일 대비 증감율%' or '0%'
+ */
+export function getSignedChangeRate(scr: number | undefined) {
+  const _scr = scr?.toLocaleString('ko-KR') + '%'
+
+  if (scr && scr > 0) {
+    return `+${_scr}`
+  }
+
+  return _scr
+}
