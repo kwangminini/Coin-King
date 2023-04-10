28fb9854f1126b64132658d870cc2146f61df288
kwangminini <rhkdals7362@gmail.com>
2023-04-11 07:46:55 +0900

    feat: falsy한 값 중 0은 허용

--- src/components/CoinCard/PriceBox/index.tsx
+++ src/components/CoinCard/PriceBox/index.tsx
@@ -14,14 +14,14 @@ export default function PriceBox({ coinId }: IProp) {
   return (
     <div className="ml-auto flex flex-col items-end">
       <p className={`text-base font-semibold ${getTextColor(coinState)}`}>
-        {coinState.tp ? (
+        {coinState.tp != null ? (
           coinState.tp?.toLocaleString('ko-KR')
         ) : (
           <Skeleton width={100} height={20} count={1} />
         )}
       </p>
       <p className={`font-semibold text-sm ${getTextColor(coinState)}`}>
-        {coinState.scr ? (
+        {coinState.scr != null ? (
           `${coinState.scr}%`
         ) : (
           <Skeleton width={50} height={20} count={1} />
