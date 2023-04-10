9761964d091a4be28d3de0f2a528fa689b9bed2e
kwangminini <rhkdals7362@gmail.com>
2023-04-11 07:42:16 +0900

    refactor: Row와 Cell 컴포넌트를 List Compound Component 형태로 리팩토링

--- src/components/common/List/index.tsx
+++ src/components/common/List/index.tsx
@@ -1,3 +1,6 @@
+import Cell from '@/components/common/List/Cell'
+import Row from '@/components/common/List/Row'
+
 interface IProp {
   children: React.ReactNode
   style?: string
@@ -6,3 +9,6 @@ interface IProp {
 export default function List({ children, style }: IProp) {
   return <ul className={`w-full ${style ? style : ''}`}>{children}</ul>
 }
+
+List.Row = Row
+List.Cell = Cell
