38b565a4e14b54fc473ff01cb65b5db961966e8f
kwangminini <rhkdals7362@gmail.com>
2023-04-12 00:00:31 +0900

    comment: 주석 추가

--- src/components/CoinCard/index.tsx
+++ src/components/CoinCard/index.tsx
@@ -5,6 +5,7 @@ import PriceBox from '@/components/CoinCard/PriceBox'
 export default function CoinCard({ icon, darkIcon, name, unit, id }: ICoin) {
   return (
     <div className="w-full max-w-500 h-80 border-[#eee] border shadow-[0_8px_16px_rgba(0,0,0,0.16)] rounded-xl px-36 flex items-center">
+      {/**일반모드일 때 아이콘 */}
       <Image
         src={icon}
         alt={id}
@@ -12,6 +13,7 @@ export default function CoinCard({ icon, darkIcon, name, unit, id }: ICoin) {
         height={34}
         className="dark:hidden"
       />
+      {/**다크모드일 때 아이콘 */}
       <Image
         src={darkIcon}
         alt={id}
