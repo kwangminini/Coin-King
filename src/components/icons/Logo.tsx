57557d4c191ddb0362af46f231ec39581eb045fa
kwangminini <rhkdals7362@gmail.com>
2023-04-11 22:55:25 +0900

    feat: 로고에 링크 추가

--- src/components/icons/Logo.tsx
+++ src/components/icons/Logo.tsx
@@ -1,5 +1,10 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Logo() {
-  return <Image src={'/logo.svg'} alt="logo" width={76} height={32} />
+  return (
+    <Link href={'/'}>
+      <Image src={'/logo.svg'} alt="logo" width={76} height={32} />
+    </Link>
+  )
 }
