b104566beed87b7dc19ce231ef5b6537483e1e8c
kwangminini <rhkdals7362@gmail.com>
2023-04-16 03:28:24 +0900

    feat: image min-width 추가

--- src/components/icons/Logo.tsx
+++ src/components/icons/Logo.tsx
@@ -4,7 +4,13 @@ import Link from 'next/link'
 export default function Logo() {
   return (
     <Link href={'/'}>
-      <Image src={'/logo.svg'} alt="logo" width={76} height={32} />
+      <Image
+        src={'/logo.svg'}
+        alt="logo"
+        width={76}
+        height={32}
+        className="min-w-76"
+      />
     </Link>
   )
 }
