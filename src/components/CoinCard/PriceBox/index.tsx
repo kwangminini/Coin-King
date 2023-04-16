8da1319bbc1dd22139e841aa0b6a44096d91e59f
kwangminini <rhkdals7362@gmail.com>
2023-04-16 15:57:09 +0900

    feat: 코인카드에 blink 효과 추가

--- src/components/CoinCard/PriceBox/index.tsx
+++ src/components/CoinCard/PriceBox/index.tsx
@@ -2,6 +2,7 @@
 
 import useGetCoinState from '@/hooks/useGetCoinState'
 import { getTextColor } from '@/utils/coinUtil/getTextColor'
+import { useEffect, useRef } from 'react'
 import Skeleton from 'react-loading-skeleton'
 
 interface IPriceBoxProps {
@@ -10,9 +11,26 @@ interface IPriceBoxProps {
 
 export default function PriceBox({ coinId }: IPriceBoxProps) {
   const { coinState } = useGetCoinState(coinId)
+  const divRef = useRef<HTMLDivElement>(null)
 
+  useEffect(() => {
+    const timer = handleBlinking()
+
+    return () => clearTimeout(timer)
+  }, [coinState.tp])
+
+  const handleBlinking = () => {
+    if (divRef.current) {
+      divRef.current.classList.add('blinking')
+    }
+    const timer = setTimeout(() => {
+      divRef.current!.classList.remove('blinking')
+    }, 500)
+
+    return timer
+  }
   return (
-    <div className="ml-auto flex flex-col items-end">
+    <div className="ml-auto flex flex-col items-end" ref={divRef}>
       <p className={`text-base font-semibold ${getTextColor(coinState)}`}>
         {coinState.tp != null ? (
           coinState.tp?.toLocaleString('ko-KR')
