ec20e24a98303a2fd6f6b79dbe72f8d6ffb6dbf9
kwangminini <rhkdals7362@gmail.com>
2023-04-16 15:29:55 +0900

    feat: color classname 외 hex로도 반환

--- src/utils/coinUtil/getSignedChangeRate.ts
+++ src/utils/coinUtil/getSignedChangeRate.ts
@@ -1,9 +1,9 @@
 /**
  * 전일 대비 증감율 가공 함수
- * @param {number | undefined} scr - 전일 대비 증감율
+ * @param {number} scr - 전일 대비 증감율
  * @returns '+전일 대비 증감율%' or '-저일 대비 증감율%' or '0%'
  */
-export function getSignedChangeRate(scr: number | undefined): string {
+export function getSignedChangeRate(scr: number): string {
   const _scr = scr?.toLocaleString('ko-KR') + '%'
 
   if (scr && scr > 0) {
--- src/utils/coinUtil/getTextColor.ts
+++ src/utils/coinUtil/getTextColor.ts
@@ -1,16 +1,23 @@
 import { ICoinState } from '@/atoms/coinsAtom'
+import { decreaseColor, increaseColor, mainFontColor } from '@/constants/color'
 
 /**
  * 전일대비 비교 값을
  * @param {ICoinState} coinState - 코인 정보
- * @returns 'text-increase-color' | 'text-decrease-color' | 'text-main-text-color'
+ * @param {boolean} hex - color를 hex 값으로 받을지 유무
+ * @returns 'text-increase-color'
+ *        | 'text-decrease-color'
+ *        | 'text-main-text-color'
+ *        | '#C84A31'
+ *        \ '#1261C4'
+ *        \ '#333333'
  */
-export function getTextColor(coinState: ICoinState): string {
+export function getTextColor(coinState: ICoinState, hex = false): string {
   if (coinState.c === 'RISE') {
-    return 'text-increase-color'
+    return hex ? increaseColor : 'text-increase-color'
   } else if (coinState.c === 'FALL') {
-    return 'text-decrease-color'
+    return hex ? decreaseColor : 'text-decrease-color'
   } else {
-    return 'text-main-text-color'
+    return hex ? mainFontColor : 'text-main-text-color'
   }
 }
