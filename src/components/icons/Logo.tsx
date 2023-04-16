986a1b0121a610db4f002f1cbd41837fbe77c231
kwangminini <rhkdals7362@gmail.com>
2023-04-16 20:30:51 +0900

    feat: Logo icon에 classname props 추가

--- src/components/icons/Logo.tsx
+++ src/components/icons/Logo.tsx
@@ -1,19 +1,19 @@
 import Image from 'next/image'
-import Link from 'next/link'
 
-export default function Logo({
-  width,
-  height,
-}: {
+interface ILogoProps extends React.HTMLProps<HTMLImageElement> {
   width?: number
   height?: number
-}) {
+  className?: string
+}
+
+export default function Logo({ width, height, className }: ILogoProps) {
   return (
     <Image
       src={'/logo.svg'}
       alt="logo"
       width={width ? width : 76}
       height={height ? height : 32}
+      className={className}
     />
   )
 }
