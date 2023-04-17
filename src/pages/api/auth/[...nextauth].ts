eb569b4839e439f57ce913887610a4f2ad6f6356
kwangminini <rhkdals7362@gmail.com>
2023-04-18 02:19:52 +0900

    feat: session 로그인 정보 서버에서 확인

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,6 @@
-import NextAuth from 'next-auth'
+import NextAuth, { NextAuthOptions } from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
-export const authOptions = {
+export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
@@ -11,6 +11,13 @@ export const authOptions = {
   ],
   pages: {
     signIn: '/auth/signin',
+  },
+  callbacks: {
+    async session({ session }) {
+      // Send properties to the client, like an access_token and user id from a provider.
+      // console.log('session::', session)
+      return session
+    },
   },
 }
 export default NextAuth(authOptions)
