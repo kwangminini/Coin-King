6d8abfa79ef24690dc4d4fbf43ee7acaf6b107cd
kwangminini <rhkdals7362@gmail.com>
2023-05-29 21:18:38 +0900

    refactor: add custom style

--- src/components/common/Input/DefaultInput.tsx
+++ src/components/common/Input/DefaultInput.tsx
@@ -7,11 +7,12 @@ export interface IDefaultInputProps
 }
 export default function DefaultInput({
   register,
+  className,
   ...props
 }: IDefaultInputProps) {
   return (
     <input
-      className="w-full text-right p-8 box-border focus:outline-none font-semibold max-h-38 border border-[#DFE0E5] rounded-sm"
+      className={`w-full text-right p-8 box-border focus:outline-none font-semibold max-h-38 border border-[#DFE0E5] rounded-sm ${className}`}
       {...register}
       {...props}
     />
