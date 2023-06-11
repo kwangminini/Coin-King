f9c37e4c535c2617d8d1572006152ff1f2fa3858
kwangminini <rhkdals7362@gmail.com>
2023-06-12 02:34:12 +0900

    feat: add backend api

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -2,6 +2,7 @@ import axios, { AxiosRequestConfig } from 'axios'
 
 const UPBIT_BASE_URL = 'https://api.upbit.com/v1/'
 const BITHUMB_BASE_URL = 'https://api.bithumb.com/public/candlestick'
+const BASE_URL = 'api'
 const axiosInstance = (url: string, options?: AxiosRequestConfig) => {
   const instance = axios.create({
     baseURL: url,
@@ -12,3 +13,4 @@ const axiosInstance = (url: string, options?: AxiosRequestConfig) => {
 
 export const upbitAPI = axiosInstance(UPBIT_BASE_URL)
 export const bithumbAPI = axiosInstance(BITHUMB_BASE_URL)
+export const API = axiosInstance(BASE_URL)
