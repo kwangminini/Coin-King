b1cdd4b05091dd50ce8d3a652101e0c62ce40638
kwangminini <rhkdals7362@gmail.com>
2023-08-03 01:32:28 +0900

    feat: change border color

--- src/components/common/Modal/AlertModal.tsx
+++ src/components/common/Modal/AlertModal.tsx
@@ -7,7 +7,7 @@ interface IAlertModalProps {
 }
 export default function AlertModal({ title, text }: IAlertModalProps) {
   return ReactDOM.createPortal(
-    <div className="z-[100] rounded border border-main-border-color fixed left-1/2 top-1/2 py-24 px-80 bg-white translate-x-[-50%] translate-y-[-50%] text-center">
+    <div className="z-[100] rounded border border-[#999] fixed left-1/2 top-1/2 py-24 px-80 bg-white translate-x-[-50%] translate-y-[-50%] text-center">
       <p className="text-20 font-bold text-center">{title}</p>
       {text.map((textItem) => (
         <p className="text-16 text-center my-14" key={textItem}>
