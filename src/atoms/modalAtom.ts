314431e7457ed441724910d0d9ff8ec21e726b0d
kwangminini <rhkdals7362@gmail.com>
2023-08-05 14:16:58 +0900

    feat: add title to alertAtom

--- src/atoms/modalAtom.ts
+++ src/atoms/modalAtom.ts
@@ -3,11 +3,13 @@ import { atom } from 'recoil'
 interface IAlertAtom {
   isShow: boolean
   message: string
+  title: string
 }
 export const alertAtom = atom<IAlertAtom>({
   key: 'alertAtom',
   default: {
     isShow: false,
     message: '',
+    title: '',
   },
 })
