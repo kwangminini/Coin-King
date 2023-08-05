9a0b1582f3766ef661be8bda514736422698d89f
kwangminini <rhkdals7362@gmail.com>
2023-08-05 14:19:26 +0900

    fix: change string to string array in text type

--- src/atoms/modalAtom.ts
+++ src/atoms/modalAtom.ts
@@ -2,14 +2,14 @@ import { atom } from 'recoil'
 
 interface IAlertAtom {
   isShow: boolean
-  message: string
+  text: string[]
   title: string
 }
 export const alertAtom = atom<IAlertAtom>({
   key: 'alertAtom',
   default: {
     isShow: false,
-    message: '',
+    text: [],
     title: '',
   },
 })
