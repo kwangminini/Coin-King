0a0d085391a23afdab8b333bbc3ab51d549a4198
kwangminini <rhkdals7362@gmail.com>
2023-08-17 23:51:20 +0900

    refactor: update userAmount api directory path

--- src/api/userAmount.ts
+++
@@ -1,6 +0,0 @@
-import { API } from '@/api/axiosInstance'
-
-export const getUserAmount = async (): Promise<{ amount: number }> => {
-  const response = await API.get('user/amount')
-  return response.data
-}
